63fc41623459c9f64f092498cbfdbc0f7eba4ee5
praneershrest <51378040+praneershrest@users.noreply.github.com>
2024-08-09 14:31:02 -0700

    splash screen skeleton (#3)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,41 @@
 import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect, useState } from 'react'
+
+/* Prevent the splash screen from auto-hiding before asset loading is complete.
+ * Call this in global scope without awaiting, otherwise this might be called too late, when the splash screen is already hidden.
+ */
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+  const [appIsReady, setAppIsReady] = useState<boolean>(false)
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // TODO: Load any resources or data that we need prior to rendering the app
+
+        // Artificially delay for two seconds to simulate a slow loading
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+      } catch (e) {
+        console.warn(e)
+      } finally {
+        setAppIsReady(true)
+      }
+    }
+    prepare()
+  }, [])
+
+  useEffect(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync()
+    }
+  }, [appIsReady])
+
+  if (!appIsReady) {
+    return null
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" />
