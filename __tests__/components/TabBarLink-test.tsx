96b6a8eceacaee195ef50da55cc462a96d2aa0ff
praneershrest <51378040+praneershrest@users.noreply.github.com>
2024-08-12 23:42:56 -0700

    use touchableopacity (#13)

--- __tests__/components/TabBarLink-test.tsx
+++ __tests__/components/TabBarLink-test.tsx
@@ -1,81 +1,36 @@
 import React from 'react'
 import { Text } from 'react-native'
-import { render, fireEvent } from '@testing-library/react-native'
+import { render } from '@testing-library/react-native'
 import { Link } from 'expo-router'
-
 import TabBarLink from '@/components/TabBarLink'
-import { Colors } from '@/constants/Colors'
 
-// Mocking the Link component from expo-router
 jest.mock('expo-router', () => ({
-  Link: jest.fn(({ href, children }) => <>{children}</>),
+  Link: jest.fn(({ children }) => children),
 }))
 
-describe('TabBarLink Component', () => {
-  it('should pass the correct href to Link component', () => {
-    const href = '/home/settings'
-    render(
-      <TabBarLink href={href}>
-        <Text>Settings</Text>
-      </TabBarLink>,
-    )
-    expect(Link).toHaveBeenCalledWith(expect.objectContaining({ href }), {})
+describe('TabBarLink', () => {
+  const hrefMock = '/home'
+  const childrenMock = <Text testID="child">Child</Text>
+
+  it('renders correctly', () => {
+    const { getByTestId } = render(<TabBarLink href={hrefMock}>{childrenMock}</TabBarLink>)
+    expect(getByTestId('child')).toBeTruthy()
   })
 
-  it('should trigger onPress when Pressable is pressed', () => {
-    const mockOnPress = jest.fn()
-    const { getByText } = render(
+  it('passes the correct href to Link component', () => {
+    render(<TabBarLink href={hrefMock}>{childrenMock}</TabBarLink>)
+    expect(Link).toHaveBeenCalledWith(
+      expect.objectContaining({ href: hrefMock, asChild: true }),
+      {},
+    )
+  })
+
+  it('wraps children with TouchableOpacity', () => {
+    const { getByTestId } = render(
       <TabBarLink href="/home">
-        <Text onPress={mockOnPress}>Home</Text>
+        <Text>Test Child</Text>
       </TabBarLink>,
     )
-    // Simulate press event
-    fireEvent.press(getByText('Home'))
-    // Ensure that the onPress handler is called
-    expect(mockOnPress).toHaveBeenCalledTimes(1)
-  })
-
-  it('should render children inside Pressable component', () => {
-    const { getByText } = render(
-      <TabBarLink href="/reels">
-        <Text>Reels</Text>
-      </TabBarLink>,
-    )
-    // Check if the children are rendered inside the Pressable component
-    expect(getByText('Reels')).toBeTruthy()
-  })
-
-  it('should change color when Pressable is pressed in and revert on press out', () => {
-    const { getByText } = render(
-      <TabBarLink href="/home">
-        <Text>Home</Text>
-      </TabBarLink>,
-    )
-
-    // Simulate press in event
-    fireEvent(getByText('Home'), 'pressIn')
-    expect(getByText('Home').props.color).toBe(Colors.light.tabIconDefault)
-
-    // Simulate press out event
-    fireEvent(getByText('Home'), 'pressOut')
-    expect(getByText('Home').props.color).toBeUndefined() // Should revert to original style
-  })
-
-  it('should render modified children when pressed', () => {
-    const { getByText } = render(
-      <TabBarLink href="/home/messages">
-        <Text>Messages</Text>
-      </TabBarLink>,
-    )
-
-    // Simulate press in to modify child
-    fireEvent(getByText('Messages'), 'pressIn')
-    // Check that the child is modified (color changed)
-    expect(getByText('Messages').props.color).toBe(Colors.light.tabIconDefault)
-
-    // Simulate press out to revert back
-    fireEvent(getByText('Messages'), 'pressOut')
-    // Check that the child reverts back to original state
-    expect(getByText('Messages').props.color).toBeUndefined()
+    expect(getByTestId('TabBarLink-TouchableOpacity')).toBeTruthy()
   })
 })
--- components/TabBarLink.tsx
+++ components/TabBarLink.tsx
@@ -1,8 +1,6 @@
-import { Pressable } from 'react-native'
-import React, { Children, useState } from 'react'
+import { TouchableOpacity } from 'react-native'
+import React from 'react'
 import { Href, Link } from 'expo-router'
-
-import { Colors } from '@/constants/Colors'
 
 interface TabBarLinkIconProps {
   href: Href<string>
@@ -10,27 +8,9 @@ interface TabBarLinkIconProps {
 }
 
 const TabBarLink = ({ href, children }: TabBarLinkIconProps) => {
-  const [isPressed, setIsPressed] = useState(false)
-
-  const handleOnPressIn = () => {
-    setIsPressed(true)
-  }
-
-  const handleOnPressOut = () => {
-    setIsPressed(false)
-  }
-
-  const pressedChildren = Children.map(children, (child) =>
-    React.cloneElement(child, {
-      color: Colors.light.tabIconDefault,
-    }),
-  )
-
   return (
     <Link href={href} asChild>
-      <Pressable onPressIn={handleOnPressIn} onPressOut={handleOnPressOut}>
-        {isPressed ? pressedChildren : children}
-      </Pressable>
+      <TouchableOpacity testID="TabBarLink-TouchableOpacity">{children}</TouchableOpacity>
     </Link>
   )
 }
