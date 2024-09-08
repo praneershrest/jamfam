142871ea0be82fa2c08df7c25da0a0eb182e3b18
Joshua Peng <46659122+joshpeng1999@users.noreply.github.com>
2024-09-08 15:50:51 -0700

    Attachment (#25)
    * update changes to test on mobile on my laptop
    
    * Completed initial Attachment component
    
    * add size matters to component
    
    * add clarifying comments
    
    * add extra clarifying comment
    
    * fix merge conflicts
    
    * address changes
    
    * change resizemode bug
    
    * fix prettier CRLF -> LF
    
    ---------
    
    Co-authored-by: Praneer Shrestha <praneer.shrest@gmail.com>

--- app/(tabs)/create/index.tsx
+++ app/(tabs)/create/index.tsx
@@ -1,12 +1,24 @@
-import { Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 import TabSafeAreaView from '@/components/TabSafeAreaView'
+import Attachment from '@/components/Attachment'
+import { AttachmentType } from '@/constants/types'
 
+// Will be making changes to this component but just having it here to see the changes
 const Create = () => {
+  const [attachment, setAttachment] = useState<AttachmentType | null>(null)
+
   return (
     <TabSafeAreaView>
-      <Text>Create</Text>
+      <Attachment
+        width={'100%'}
+        height={400}
+        resizeMode="contain"
+        mode="reel"
+        attachment={attachment}
+        btnText="Upload Profile"
+        setAttachment={setAttachment}
+      />
     </TabSafeAreaView>
   )
 }
