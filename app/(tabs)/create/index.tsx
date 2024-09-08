f7f4b5e2e1d154bd521a7e9bf915abcfd62ea6f4
Joshua Peng <jpa95@sfu.ca>
2024-09-07 22:24:17 -0700

    Completed initial Attachment component

--- app/(tabs)/create/index.tsx
+++ app/(tabs)/create/index.tsx
@@ -4,13 +4,22 @@ import React, { useState } from 'react'
 import TabSafeAreaView from '@/components/TabSafeAreaView'
 import Attachment from '@/components/Attachment'
 import { AttachmentType } from '@/constants/types'
+import { Colors } from '@/constants/Colors'
 
 const Create = () => {
   const [attachment, setAttachment] = useState<AttachmentType | null>(null)
 
   return (
     <TabSafeAreaView>
-      <Attachment width={'100%'} height={200} attachment={attachment} btnText="Upload Audio / Video / Image" setAttachment={setAttachment}/>
+      <Attachment
+        width={'100%'}
+        height={400}
+        resizeMode="contain"
+        mode="reel"
+        attachment={attachment}
+        btnText="Upload Profile"
+        setAttachment={setAttachment}
+      />
     </TabSafeAreaView>
   )
 }
--- components/Attachment.tsx
+++ components/Attachment.tsx
@@ -1,194 +1,442 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 import {
-    View,
-    Text,
-    StyleSheet,
-    TouchableHighlight,
-    StyleProp,
-    ViewStyle, 
-    useWindowDimensions,
-    useColorScheme,
-    Alert,
-  } from 'react-native'
-import { Dispatch, SetStateAction } from 'react';
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+  StyleProp,
+  ViewStyle,
+  Image,
+  useColorScheme,
+  Alert,
+  Pressable,
+} from 'react-native'
+import { Video, Audio } from 'expo-av'
 import { Colors } from '@/constants/Colors'
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient'
 import VectorIcon from './VectorIcon'
 import { ButtonSize } from '@/constants/Size'
-import * as ImagePicker from 'expo-image-picker';
-import * as DocumentPicker from 'expo-document-picker';
-  
-  type AttachmentType = {
-    type: 'video' | 'audio' | 'image';
-    uri: string;  // Path or URI to the attachment
-    name?: string;  // Optional name for the attachment
-  };
+import * as ImagePicker from 'expo-image-picker'
+import * as DocumentPicker from 'expo-document-picker'
+import { AttachmentType } from '@/types/attachmentType'
+import { useFocusEffect } from '@react-navigation/native'
 
-  type Props = React.ComponentPropsWithRef<typeof View> & {
-    /**
-     * Initials to show as the text in the `Avatar`.
-     */
-    backgroundColor?: string
-    /**
-     * Custom width for the button for the container.
-     */
-    width?: number
-    /**
-     * Custom height for the button for the container.
-     */
-    height?: number
-    /**
-     * Custom text for the button.
-     */
-    btnText?: string  
-    /**
-     * Style for the container
-     */
-    style?: StyleProp<ViewStyle>
-    /**
-     * Image/Video/Audio that will be used to post
-     */
-    attachment: AttachmentType | null
-
-    setAttachment: Dispatch<SetStateAction<AttachmentType | null>>
-  }
-  
+type Props = React.ComponentPropsWithRef<typeof View> & {
   /**
-   * Used for attaching a image/video.
-   *
-   * ## Usage
-   * ```
-   * import * as React from 'react';
-   * import * as Avatar from '@/components/Avatar'
-   *
-   * const MyComponent = () => (
-   *   <Avatar.Text size={24} label="XD" />
-   * );
-   * ```
+   * Custom background color for the container.
    */
-  const Attachment = ({
-    width = ButtonSize.defaultWidth,
-    height = ButtonSize.defaultHeight,
-    btnText,
-    attachment,
-    style,
-    setAttachment,
-  }: Props) => {
-    const { ...restStyle } = StyleSheet.flatten(style) || {}
-    const { fontScale } = useWindowDimensions()
-    const colorScheme = useColorScheme;
-      // Determine the gradient and underlay colors based on the theme
-    const gradientColors = colorScheme === 'light'
-    ? [Colors.buttonGradientLight.primary, Colors.buttonGradientLight.secondary]
-    : [Colors.buttonGradientDark.primary, Colors.buttonGradientDark.secondary];
+  backgroundColor?: string
+  /**
+   * Width of the attachment container.
+   */
+  width?: number
+  /**
+   * Height of the attachment container.
+   */
+  height?: number
+  /**
+   * Border radius for the attachment container.
+   */
+  borderRadius?: number
+  /**
+   * Text to display on the button when no attachment is selected.
+   */
+  btnText?: string
+  /**
+   * Style for the attachment container.
+   */
+  style?: StyleProp<ViewStyle>
+  /**
+   * The attachment object, which includes type and URI.
+   */
+  attachment: AttachmentType | null
+  /**
+   * Function to update the attachment state.
+   */
+  setAttachment: React.Dispatch<React.SetStateAction<AttachmentType | null>>
+  /**
+   * How the media (image or video) should be resized when rendered.
+   */
+  resizeMode: string
+  /**
+   * The mode of the component, determining where you're using the component (e.g., 'reel', 'profile', or 'cover' (as in cover photo)).
+   */
+  mode: string
+}
 
-    const underlayColor = colorScheme === 'light'
-      ? Colors.buttonGradientLight.underlay
-      : Colors.buttonGradientDark.underlay;
+const defaultImageUri = 'https://via.placeholder.com/300x200' //TO-DO change this to whatever we decide the default image will
 
-    const iconColor = colorScheme === 'light'
-      ? Colors.light.icon
-      : Colors.dark.icon;
+/**
+ * Attachment is for any area where the user needs to upload files. This includes audio, video for reels, or image files for profile
+ *
+ * ## Usage
+ *
+ *      <Attachment
+ *       width={'100%'}
+ *       height={400}
+ *       resizeMode="contain"
+ *       mode="reel"
+ *       attachment={attachment}
+ *       btnText="Upload Profile"
+ *       setAttachment={setAttachment}
+ *     />
+ *
+ */
 
-    const textColor = colorScheme === 'light'
-      ? Colors.light.text
-      : Colors.dark.text; 
-      
+const Attachment = ({
+  width = ButtonSize.defaultWidth,
+  height = ButtonSize.defaultHeight,
+  backgroundColor = Colors.white,
+  borderRadius = 0,
+  btnText,
+  attachment,
+  setAttachment,
+  resizeMode = 'contain',
+  mode = 'reel',
+}: Props) => {
+  // Controlling the video async
+  const videoRef = useRef<Video>(null)
+  //for light mode dark mode
+  const colorScheme = useColorScheme()
+  const [audioRef, setAudioRef] = useState<Audio.Sound | null>(null)
+  const [isPlaying, setIsPlaying] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
-      const selectAttachment = async (
-        setAttachment: Dispatch<SetStateAction<AttachmentType | null>>
-      ) => {
-        // Ask the user what type of attachment they want to select
-        Alert.alert(
-          'Select Attachment',
-          'Choose the type of attachment you want to add',
-          [
-            {
-              text: 'Image or Video',
-              onPress: async () => {
-                const result = await ImagePicker.launchImageLibraryAsync({
-                  mediaTypes: ImagePicker.MediaTypeOptions.All, // Allows both images and videos
-                  quality: 1,
-                });
-      
-                if (!result.canceled) {
-                  const { uri, type, fileName } = result;
-                  setAttachment({
-                    type: result.type === 'video' ? 'video' : 'image',
-                    uri: uri!,
-                    name: fileName || 'attachment',  // ImagePicker doesn't always provide a file name
-                  });
-                }
-              },
-            },
-            {
-              text: 'Audio',
-              onPress: async () => {
-                const result = await DocumentPicker.getDocumentAsync({
-                  type: 'audio/*',
-                });
-      
-                if (result.type !== 'cancel') {
-                  setAttachment({
-                    type: 'audio',
-                    uri: result.uri,
-                    name: result.name,
-                  });
-                }
-              },
-            },
-            {
-              text: 'Cancel',
-              style: 'cancel',
-            },
-          ],
-          { cancelable: true }
-        );
-      };
+  const gradientColors =
+    colorScheme === 'light'
+      ? [Colors.buttonGradientLight.primary, Colors.buttonGradientLight.secondary]
+      : [Colors.buttonGradientDark.primary, Colors.buttonGradientDark.secondary]
 
-    return (
-      <LinearGradient
-        colors={gradientColors} // Gradient colors from orange to yellow
-        start={{ x: 0, y: 0.5 }}  // Starting point (top-left)
-        end={{ x: 0.5, y: 1 }}    // Ending point (bottom-right)
-        locations={[0, 0.8]}    // Emphasize the primary color
-        style={[{ borderRadius: 5,   }, restStyle]}
-        >
-        <TouchableHighlight
-            style={{
-                padding: 20,
-                borderRadius: 5,
-                width: width, height: height
-            }}
-             underlayColor= {underlayColor}
-             onPress={() => selectAttachment(setAttachment)}>
-            <View style={styles.buttonContainer}>
-                <VectorIcon
-                    name="upload"
-                    type="MaterialIcons"
-                    color={iconColor}
-                    size={100}
-                />
-                <Text style={[styles.text, {
-                  color: textColor,
-                  fontSize: width / 4,
-                },]}>{btnText}</Text>
-            </View>
-        </TouchableHighlight>
-      </LinearGradient>
-    )
-  }
-  
-  
-  const styles = StyleSheet.create({
-    text: {
-      textAlign: 'center',
-      textAlignVertical: 'center',
-    },
-    buttonContainer: {
-        justifyContent: 'center',  // Aligns content vertically in the center
-        alignItems: 'center',       // Aligns content horizontally in the center
+  const iconColor = colorScheme === 'light' ? Colors.light.icon : Colors.dark.icon
+  const textColor = colorScheme === 'light' ? Colors.light.text : Colors.dark.text
+
+  // Used just for controlling the play button based on if it's audio or a video
+  const handlePlayPress = async () => {
+    if (attachment.type === 'video') {
+      if (isPlaying) {
+        await videoRef.current.pauseAsync()
+      } else {
+        await videoRef.current.playAsync()
+      }
+    } else if (attachment.type === 'audio') {
+      if (!audioRef.current) {
+        const { sound } = await Audio.Sound.createAsync(
+          { uri: attachment.uri },
+          { shouldPlay: false },
+        )
+        audioRef.current = sound
+      }
+      if (isPlaying) {
+        await audioRef.current.pauseAsync()
+      } else {
+        await audioRef.current.playAsync()
+      }
     }
-  })
-  
-  export default Attachment
-  
+    setIsPlaying(!isPlaying)
+  }
+
+  useEffect(() => {
+    return () => {
+      // When cancelling the audio upload, destroy audio component.
+      if (audioRef && audioRef.current) {
+        audioRef.current.unloadAsync()
+      }
+    }
+  }, [audioRef])
+
+  const selectAttachment = async (
+    setAttachment: React.Dispatch<React.SetStateAction<AttachmentType | null>>,
+    mode: string,
+  ) => {
+    setIsLoading(true)
+
+    let alertOptions = []
+    let msg = ''
+
+    // Adjust the options based on the mode
+    // i.e. if it's a reel, then only video and audio is allowed, if it's profile pic, then only image is allowed.
+    if (mode === 'reel') {
+      msg = 'Choose the type of attachment you want to add'
+      alertOptions = [
+        {
+          text: 'Video',
+          onPress: async () => {
+            const result = await ImagePicker.launchImageLibraryAsync({
+              mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+              quality: 1,
+            })
+
+            if (!result.canceled && result.assets && result.assets.length > 0) {
+              const { uri, fileName } = result.assets[0]
+              setAttachment({
+                type: 'video',
+                uri: uri,
+                name: fileName || 'attachment',
+              })
+            } else {
+              // toggle the loading screen to be off if nothing is selected
+              setIsLoading(false)
+            }
+          },
+        },
+        {
+          text: 'Audio',
+          onPress: async () => {
+            const result = await DocumentPicker.getDocumentAsync({
+              type: 'audio/*',
+            })
+            if (!result.canceled && result.assets && result.assets.length > 0) {
+              const { uri, fileName } = result.assets[0]
+              setAttachment({
+                type: 'audio',
+                uri: uri,
+                name: fileName,
+              })
+              const { sound } = await Audio.Sound.createAsync({ uri: uri }, { shouldPlay: false })
+              setAudioRef(sound)
+            } else {
+              // toggle the loading screen to be off if nothing is selected
+              setIsLoading(false)
+            }
+          },
+        },
+      ]
+    } else if (mode === 'profile') {
+      msg = 'Choose an image for your profile from the gallery?'
+      alertOptions = [
+        {
+          text: 'Image',
+          onPress: async () => {
+            const result = await ImagePicker.launchImageLibraryAsync({
+              mediaTypes: ImagePicker.MediaTypeOptions.Images,
+              quality: 1,
+            })
+
+            if (!result.canceled && result.assets && result.assets.length > 0) {
+              const { uri, fileName } = result.assets[0]
+              setAttachment({
+                type: 'image',
+                uri: uri,
+                name: fileName || 'attachment',
+              })
+            } else {
+              // toggle the loading screen to be off if nothing is selected
+              setIsLoading(false)
+            }
+          },
+        },
+      ]
+    } else if (mode === 'cover') {
+      msg = 'Choose the type of attachment you want to add'
+      alertOptions = [
+        {
+          text: 'Image or Video',
+          onPress: async () => {
+            const result = await ImagePicker.launchImageLibraryAsync({
+              mediaTypes: ImagePicker.MediaTypeOptions.All,
+              quality: 1,
+            })
+
+            if (!result.canceled && result.assets && result.assets.length > 0) {
+              const { uri, type, fileName } = result.assets[0]
+              setAttachment({
+                type: type === 'video' ? 'video' : 'image',
+                uri: uri,
+                name: fileName || 'attachment',
+              })
+            } else {
+              // toggle the loading screen to be off if nothing is selected
+              setIsLoading(false)
+            }
+          },
+        },
+      ]
+    }
+    // For if the user decides not to select anything and presses cancel instead
+    alertOptions.push({
+      text: 'Cancel',
+      style: 'cancel',
+      onPress: async () => {
+        setIsLoading(false)
+      },
+    })
+
+    Alert.alert('Select Attachment', msg, alertOptions, {
+      cancelable: true,
+    })
+  }
+
+  // Used for when the cancel button is pressed
+  const resetAttachment = async () => {
+    setAttachment(null)
+    setIsPlaying(false)
+    setIsLoading(false)
+    if (audioRef) {
+      await audioRef.current.pauseAsync()
+      await audioRef.current.unloadAsync()
+      setAudioRef(null)
+    }
+  }
+
+  // The audio and video component has a callback for when the image or video finishes uploading to the app
+  const handleContentLoad = () => {
+    setIsLoading(false)
+  }
+
+  // This function is triggered if a user moves to another screen, it will disable everything
+  useFocusEffect(
+    useCallback(() => {
+      return () => {
+        if (videoRef && videoRef.current) {
+          videoRef.current.pauseAsync()
+        }
+        if (audioRef && audioRef.current) {
+          audioRef.current.pauseAsync()
+        }
+        setIsLoading(false)
+        setIsPlaying(false)
+      }
+    }, [videoRef, audioRef]),
+  )
+
+  return (
+    <View style={[{ width: width, height: height, backgroundColor: backgroundColor }]}>
+      {!attachment ? (
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => selectAttachment(setAttachment, mode)}>
+          <LinearGradient
+            colors={gradientColors}
+            start={{ x: 0, y: 0.5 }}
+            end={{ x: 0.5, y: 1 }}
+            locations={[0, 0.8]}
+            style={[{ borderRadius: borderRadius }, styles.linearGradientContainer]}>
+            {isLoading ? (
+              <ActivityIndicator
+                size="large"
+                color={Colors.primary}
+                style={styles.loadingSpinner}
+              />
+            ) : (
+              <>
+                <VectorIcon name="upload" type="MaterialIcons" color={iconColor} size={100} />
+                <Text
+                  style={[
+                    styles.text,
+                    {
+                      color: textColor,
+                    },
+                  ]}>
+                  {btnText}
+                </Text>
+              </>
+            )}
+          </LinearGradient>
+        </TouchableOpacity>
+      ) : (
+        <View style={styles.contentContainer}>
+          {attachment.type === 'image' ? (
+            <Image
+              resizeMode={resizeMode}
+              source={{ uri: attachment.uri }}
+              style={[{ borderRadius: borderRadius }, styles.mediaContainer]} // Combine the inline and external styles
+              onLoad={handleContentLoad}
+            />
+          ) : (
+            <Pressable onPress={handlePlayPress} style={styles.mediaContainer}>
+              {isLoading && (
+                <ActivityIndicator
+                  size="large"
+                  color={Colors.primary}
+                  style={styles.loadingSpinner}
+                />
+              )}
+              {attachment.type === 'video' ? (
+                <Video
+                  ref={videoRef}
+                  source={{ uri: attachment.uri }}
+                  rate={1.0}
+                  resizeMode={resizeMode}
+                  shouldPlay={isPlaying}
+                  onLoad={handleContentLoad}
+                  style={[{ borderRadius: borderRadius }, styles.media]}
+                />
+              ) : (
+                <Image
+                  resizeMode="cover"
+                  source={{ uri: defaultImageUri }}
+                  style={[{ borderRadius: borderRadius }, styles.media]}
+                  onLoad={handleContentLoad}
+                />
+              )}
+
+              {!isPlaying && (
+                <View style={styles.playButton}>
+                  <VectorIcon
+                    name="play-circle-outline"
+                    type="MaterialIcons"
+                    color={Colors.white}
+                    size={100}
+                  />
+                </View>
+              )}
+            </Pressable>
+          )}
+          <TouchableOpacity style={styles.resetButton} onPress={resetAttachment}>
+            <VectorIcon name="close" type="MaterialIcons" color="white" size={24} />
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  linearGradientContainer: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  resetButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: 15,
+    padding: 5,
+  },
+  mediaContainer: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  media: {
+    width: '100%',
+    height: '100%',
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  playButton: {
+    position: 'absolute',
+  },
+  loadingSpinner: {
+    position: 'absolute',
+  },
+})
+
+export default Attachment
