5e8a127842236eab713be19c2d38a48b6bcffcc4
joshpeng1999 <jpa95@sfu.ca>
2024-09-20 19:48:02 -0700

    change js to ts file

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -6,13 +6,13 @@ import Constants from 'expo-constants'
 
 // Initialize Firebase using environment variables
 const firebaseConfig = {
-  apiKey: Constants.expoConfig.extra.firebaseApiKey,
-  authDomain: Constants.expoConfig.extra.firebaseAuthDomain,
-  projectId: Constants.expoConfig.extra.firebaseProjectId,
-  storageBucket: Constants.expoConfig.extra.firebaseStorageBucket,
-  messagingSenderId: Constants.expoConfig.extra.firebaseMessagingSenderId,
-  appId: Constants.expoConfig.extra.firebaseAppId,
-  measurementId: Constants.expoConfig.extra.firebaseMeasurementId,
+  apiKey: Constants.expoConfig?.extra?.firebaseApiKey,
+  authDomain: Constants.expoConfig?.extra?.firebaseAuthDomain,
+  projectId: Constants.expoConfig?.extra?.firebaseProjectId,
+  storageBucket: Constants.expoConfig?.extra?.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig?.extra?.firebaseMessagingSenderId,
+  appId: Constants.expoConfig?.extra?.firebaseAppId,
+  measurementId: Constants.expoConfig?.extra?.firebaseMeasurementId,
 }
 
 const app = initializeApp(firebaseConfig)
