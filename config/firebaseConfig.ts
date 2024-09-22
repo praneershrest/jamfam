69630895973579f277fa6e9246175ea30a483463
Joshua Peng <46659122+joshpeng1999@users.noreply.github.com>
2024-09-21 22:00:51 -0700

    configure firebase and connect to app (#27)
    * configure firebase and connect to app
    
    * change js to ts file
    
    * change js to ts file

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,24 @@
+// Configs Firebase to connect to the app on start up
+import { initializeApp } from 'firebase/app'
+import { getStorage } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore'
+import Constants from 'expo-constants'
+
+// Initialize Firebase using environment variables
+const firebaseConfig = {
+  apiKey: Constants.expoConfig?.extra?.firebaseApiKey,
+  authDomain: Constants.expoConfig?.extra?.firebaseAuthDomain,
+  projectId: Constants.expoConfig?.extra?.firebaseProjectId,
+  storageBucket: Constants.expoConfig?.extra?.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig?.extra?.firebaseMessagingSenderId,
+  appId: Constants.expoConfig?.extra?.firebaseAppId,
+  measurementId: Constants.expoConfig?.extra?.firebaseMeasurementId,
+}
+
+const app = initializeApp(firebaseConfig)
+// To store photos and videos
+const storage = getStorage(app)
+// To store database elements
+const db = getFirestore(app)
+
+export { app, storage, db }
